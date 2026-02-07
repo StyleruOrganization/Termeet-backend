6553d66961f5d3e532edd347de15f8bd31557082
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-02-08 01:14:33 +0300

    chore: fix some details in configurations of db

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,23 +1,39 @@
-from dataclasses import Field
+from dataclasses import field
+
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ConfigBase(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="backend/settings/.env", env_file_encoding="utf-8", extra="ignore"
+        env_file="backend/settings/.env",
+        env_file_encoding="utf-8",
+        extra="ignore"
     )
 
 
 class DatabaseConfig(ConfigBase):
-    model_config = SettingsConfigDict(env_prefix="db_")
+    model_config = SettingsConfigDict(env_prefix="POSTGRES_")
 
-    host: str
-    password: str
+    PASSWORD: str
+    USER: str
+    DB: str
+    HOST: str
+    PORT: int
+
+    @computed_field  # Прочитал, что классная практика, помогает в сериализации
+    @property
+    def db_url(self):
+        return (f"postgresql+asyncpg://{self.USER}:{self.PASSWORD}@"
+                f"{self.HOST}:{self.PORT}/{self.DB}")
 
 
 class Config(BaseSettings):
-    db: DatabaseConfig = Field(default_factory=DatabaseConfig)
+    db: DatabaseConfig = field(default_factory=DatabaseConfig)
 
     @classmethod
     def load(cls) -> "Config":
         return cls()
+
+
+config = Config()

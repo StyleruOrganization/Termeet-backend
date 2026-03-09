6cd60862891df6f20916b71551898fb116ab356a
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-03-09 20:57:28 +0300

    feature: add schemas according schemas

---
+++ backend/src/meetings/schemas.py
@@ -0,0 +1,30 @@
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class SlotsUser(BaseModel):
+    name: str
+    slots: list[list[str]]
+
+
+class Meet(BaseModel):
+    name: str
+    description: str | None = None
+    link: str | None = None
+    duration: str | None = None
+    dataRange: list[list[str]] = Field(validation_alias="data_range")
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True
+        )
+
+
+class MeetCreate(Meet):
+    pass
+
+
+class MeetResponse(Meet):
+    hash: UUID = Field(validation_alias="id")  # ПРОВЕРЬ
+    slots: list[SlotsUser] = []

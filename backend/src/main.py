6abe86d65d945599c0bb03cd66ea45d00b233c8d
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-03-11 13:28:46 +0300

    feature: add lifespan

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.src.meetings.routers import router as meetings_router
+from backend.src.lifespan import lifespan
 
 app = FastAPI(
     title="Termeet API",
     version="1.0.0",
     description="API для Termeet",
-    root_path="/api"
+    root_path="/api",
+    lifespan=lifespan
 )
 
 origins = [

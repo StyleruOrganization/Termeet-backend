9880fd15648c7e9e3981881c5e99d9a548950c2e
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-04-10 14:34:37 +0300

    feature: add auth schemas

---
+++ backend/src/auth/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class Code(BaseModel):
+    code: str
+
+
+class AuthTokens(BaseModel):
+    access_token: str
+    refresh_token: str
+    expires_in: int
+
+
+class YandexUserData(BaseModel):
+    id: str
+    login: str
+    client_id: str
+    display_name: str
+    real_name: str
+    first_name: str
+    last_name: str
+    sex: str
+    default_email: str
+    emails: list
+    psuid: str

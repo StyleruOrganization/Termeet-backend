260f66d86c7f48d4dd1638f2c4beac5c13b38d3d
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-04-21 17:22:48 +0300

    fix: add methods repositories

--- backend/src/auth/repositories.py
+++ backend/src/auth/repositories.py
@@ -1,12 +1,24 @@
+from typing import TYPE_CHECKING
 from abc import ABC, abstractmethod
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+if TYPE_CHECKING:
+    from uuid import UUID
 
-class Repository():
+
+class Repository(ABC):
     def __init__(self, session: AsyncSession):
         self.session = session
 
     @abstractmethod
     async def register_user(self, user: dict):
+        pass
+
+    @abstractmethod
+    async def yandex_check_user_in_db(self, user: dict):
+        pass
+
+    @abstractmethod
+    async def check_user_in_db(self, id: UUID):
         pass

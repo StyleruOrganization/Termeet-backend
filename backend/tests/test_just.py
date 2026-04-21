3033f6422acbf42055bdf4009147cbd0ed9ea6d4
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-04-21 19:16:49 +0300

    infra: update DEV: CI, docker compose, Dockerfile

--- backend/tests/test_just.py
+++ backend/tests/test_just.py
@@ -9,50 +9,56 @@ from backend.src.auth.schemas import UserSchema
 from backend.src.dependencies import async_session_maker
 
 
-@pytest.mark.asyncio
-async def test_register_user(
-    session: AsyncSession = async_session_maker(),
-    user: dict = {
-        "id": "1042247364",
-        "login": "maklakov.daniils",
-        "client_id": "2421c75bbd80418ca1dacae9595c3a54",
-        "display_name": "maklakov.daniils",
-        "real_name": "Даниил Маклаков",
-        "first_name": "Даниил",
-        "last_name": "Маклаков",
-        "sex": "male",
-        "default_email": "daniil@yandex.ru",
-        "emails": ["maklakov.daniils@yandex.ru"],
-        "psuid": "1.AA_lbA.J5jcnC1xk6YXXjGyvh0dQA.nFm8MEKh81UCI0HLpkpseQ",
-    },
-):
-    query = (
-        select(Users, OAuthAccount)
-        .join(Users.oauth_accounts)
-        .where(OAuthAccount.provider_user_id == int(user["id"]))
-    )
-    result = await session.execute(query)
-    if user := (result.scalar_one_or_none()):
-        user: UserSchema = UserSchema.model_validate(user)
-        print(user)
+def test_config():
+    from backend.src.config import BASE_DIR
 
-    # object = Users(
-    #     first_name=user["first_name"],
-    #     last_name=user["last_name"],
-    #     email=user["default_email"],
-    #     additional_emails=user["emails"]
-    # )
-    # object.oauth_accounts.append(
-    #     OAuthAccount(
-    #         provider="YANDEX",
-    #         provider_user_id=int(user["id"])
-    #     )
-    # )
+    print(BASE_DIR)
 
-    # session.add(object)
-    # await session.flush()
-    # await session.rollback()
-    # await session.commit()
+
+# @pytest.mark.asyncio
+# async def test_register_user(
+#     session: AsyncSession = async_session_maker(),
+#     user: dict = {
+#         "id": "1042247364",
+#         "login": "maklakov.daniils",
+#         "client_id": "2421c75bbd80418ca1dacae9595c3a54",
+#         "display_name": "maklakov.daniils",
+#         "real_name": "Даниил Маклаков",
+#         "first_name": "Даниил",
+#         "last_name": "Маклаков",
+#         "sex": "male",
+#         "default_email": "daniil@yandex.ru",
+#         "emails": ["maklakov.daniils@yandex.ru"],
+#         "psuid": "1.AA_lbA.J5jcnC1xk6YXXjGyvh0dQA.nFm8MEKh81UCI0HLpkpseQ",
+#     },
+# ):
+#     query = (
+#         select(Users, OAuthAccount)
+#         .join(Users.oauth_accounts)
+#         .where(OAuthAccount.provider_user_id == int(user["id"]))
+#     )
+#     result = await session.execute(query)
+#     if user := (result.scalar_one_or_none()):
+#         user: UserSchema = UserSchema.model_validate(user)
+#         print(user)
+
+#     # object = Users(
+#     #     first_name=user["first_name"],
+#     #     last_name=user["last_name"],
+#     #     email=user["default_email"],
+#     #     additional_emails=user["emails"]
+#     # )
+#     # object.oauth_accounts.append(
+#     #     OAuthAccount(
+#     #         provider="YANDEX",
+#     #         provider_user_id=int(user["id"])
+#     #     )
+#     # )
+
+#     # session.add(object)
+#     # await session.flush()
+#     # await session.rollback()
+#     # await session.commit()
 
 
 # @pytest.mark.asyncio

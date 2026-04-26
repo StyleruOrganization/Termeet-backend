00968acddfa30a6e48d950d2467dd76d436223ae
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-04-26 15:55:41 +0300

    feature: add new scheamas for default auth

--- backend/src/auth/schemas.py
+++ backend/src/auth/schemas.py
@@ -25,6 +25,18 @@ class YandexUserData(BaseModel):
     psuid: str
 
 
+class RegisterUserData(BaseModel):
+    first_name: str
+    last_name: str
+    default_email: str
+    password: str
+
+
+class LoginUserData(BaseModel):
+    email: str
+    password: str
+
+
 class TokenInfo(BaseModel):
     access_token: str
     token_type: str = "Bearer"

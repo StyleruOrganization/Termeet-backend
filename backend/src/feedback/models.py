9fffe809ae354e9cc4555b25350910599b3f8133
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-05-10 17:32:52 +0300

    feature: add links users with feedback

--- backend/src/feedback/models.py
+++ backend/src/feedback/models.py
@@ -1,11 +1,16 @@
+from typing import TYPE_CHECKING, Optional
 from uuid import uuid4, UUID
 from enum import Enum
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import ENUM as PgEnum
+from sqlalchemy import ForeignKey
 
 from backend.src.models import Base
 from backend.src.feedback.schemas import FeedbackType, CommunicationChannel
+
+if TYPE_CHECKING:
+    from backend.src.users.models import Users
 
 
 class Feedback(Base):
@@ -26,3 +31,8 @@ class Feedback(Base):
     )
     contact: Mapped[str] = mapped_column(nullable=False)
     message: Mapped[str] = mapped_column(nullable=False)
+
+    user_id: Mapped[Optional[UUID]] = mapped_column(
+        ForeignKey("users.id"), nullable=True
+    )
+    user: Mapped[Optional["Users"]] = relationship(back_populates="feedbacks")

7016e505b08ab6f4248a04ea458e0205208244e5
Маклаков Даниил Павлович <dpmaklakov@edu.hse.ru>
2026-06-20 20:34:03 +0300

    feature: add RabbitMQ lifespan

--- backend/src/lifespan.py
+++ backend/src/lifespan.py
@@ -3,12 +3,30 @@ from contextlib import asynccontextmanager
 
 import yaml
 
+from backend.src.broker import RabbitMQClient
+from backend.src.config import config
+
 if TYPE_CHECKING:
     from fastapi import FastAPI
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan_docs(app: FastAPI):
     with open("backend/docs/openapi.yaml", "w", encoding="utf-8") as f:
         yaml.dump(app.openapi(), f)
     yield
+
+
+@asynccontextmanager
+async def lifespan_broker(app: FastAPI):
+    rabbit = RabbitMQClient(url=config.rabbitmq.rb_url)
+    await rabbit.connect()
+    app.state.rabbitmq = rabbit
+    yield
+    await rabbit.close()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with lifespan_docs(app), lifespan_broker(app):
+        yield
